d50d67cf370051025a5c0fd10f9d386f77c84a10
Jesper Engberg <jeengbe@gmail.com>
2023-01-08 12:09:50 +0100

    epic config handling

--- dsh/backend/src/config.ts
+++ dsh/backend/src/config.ts
@@ -1,49 +1,25 @@
-import { Parse, v, validate } from 'vality';
+import { Parse, v } from 'vality';
+import { loadEnv } from 'vality-env';
 
 const config = {
   db: {
-    url: v.string,
-    databaseName: v.string,
+    url: v.env('DATABASE_URL', v.string),
+    databaseName: v.env('DATABASE_NAME', v.string),
   },
   jwt: {
     privateKey: v.string,
   },
 };
 
-const environment = {
-  db: {
-    url: 'DATABASE_URL',
-    databaseName: 'DATABASE_NAME',
-  },
-  jwt: {
-    privateKey: 'JWT_PRIVATE_KEY',
-  },
-};
-
 export type Config = Parse<typeof config>;
 
 export function loadConfig(): Config {
-  const envConfig = populateEnvironment(environment);
+  const validatedConfig = loadEnv(config);
 
-  const validatedConfig = validate(config, envConfig);
   if (!validatedConfig.valid) {
     console.log(validatedConfig.errors);
     throw new Error('Invalid config');
   }
 
   return validatedConfig.data;
-}
-
-function populateEnvironment(env: Record<string, any>): Record<string, any> {
-  const result: Record<string, any> = {};
-
-  for (const key in env) {
-    const value = env[key];
-    if (typeof value === 'object') {
-      result[key] = populateEnvironment(value);
-    } else {
-      result[key] = process.env[value as keyof typeof process.env];
-    }
-  }
-  return result;
 }
