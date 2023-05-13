a6313e85a2a36ca313c6f816c922c8c8caff5cc3
Jesper Engberg <jeengbe@gmail.com>
2023-05-13 19:30:34 +0200

    change settings

--- src/administration/sections/persons/slices/persons/settings/action.ts
+++ src/administration/sections/persons/slices/persons/settings/action.ts
@@ -1,10 +1,19 @@
 'use server';
 
+import { requireLogin } from '#/auth/action';
 import { AccountSettings } from '#/backend/repositories/content/account';
 import { wrapAction } from '#/utils/action';
 
 export const settings = wrapAction(
   async (personId: string, settings: AccountSettings) => {
-    console.log('HO');
+    const context = await requireLogin();
+
+    const account = await context.services.account.getById(personId);
+
+    if (!account) {
+      throw new Error('ACCOUNT_NOT_FOUND');
+    }
+
+    await context.services.account.update(account.id, { settings });
   }
 );
