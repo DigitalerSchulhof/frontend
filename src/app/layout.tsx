9a940c1662a7c73a84bf9fffcb18e674a33a8f7d
Jesper Engberg <jeengbe@gmail.com>
2023-12-24 16:11:31 +0100

    fix: remove & upgrade deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,6 @@ import { getContext } from '#/auth/component';
 import React from 'react';
 import { Providers } from './providers';
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires -- Don't want this in prod
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
-
 export default async function RootLayout({
   children,
 }: {
